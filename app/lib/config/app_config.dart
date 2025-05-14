ade12c694fdb9ffa1a63303666b4c484cc501bf5
Iheb DERUICH <ideruich@sofrecom.fr>
2025-05-14 10:23:24 +0100

    fixxx

--- app/lib/config/app_config.dart
+++ app/lib/config/app_config.dart
@@ -1,4 +1,3 @@
 class AppConfig {
-  static const String apiBaseUrl =
-      'http://10.0.2.2:8080'; // Ajustez selon votre configuration
+  static const String apiBaseUrl = 'http://10.0.2.2:8080';
 }
