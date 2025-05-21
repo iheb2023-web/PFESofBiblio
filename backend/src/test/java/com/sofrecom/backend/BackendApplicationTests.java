bd2c4c05f0c4da6b8bd27f1cdf4bb14a3bf0c7bf
Mohamed El Hedi Hamdi <mohamedelhedi.hamdi@sofrecom.com>
2025-05-21 12:51:30 +0200

    config test

--- backend/src/test/java/com/sofrecom/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/sofrecom/backend/BackendApplicationTests.java
@@ -3,12 +3,20 @@ package com.sofrecom.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
+
 @SpringBootTest(classes = BackendApplication.class)
 class BackendApplicationTests {
+
+	@BeforeAll
+	static void loadEnv() {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+	}
 
 	@Test
 	void contextLoads() {
 	}
-
-
 }
+
